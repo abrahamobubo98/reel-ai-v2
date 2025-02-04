11d6e3a2f15e4108f1cf23b679375a4dec986b1f
abrahamobubo98 <84512669+abrahamobubo98@users.noreply.github.com>
2025-02-04 08:49:53 -0600

    Initial Commit

---
+++ reel-ai-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reel-ai-v2
+//
+//  Created by Abraham Obubo on 04/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reel-ai-v2Tests/reel_ai_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  reel_ai_v2Tests.swift
+//  reel-ai-v2Tests
+//
+//  Created by Abraham Obubo on 04/02/2025.
+//
+
+import Testing
+@testable import reel_ai_v2
+
+struct reel_ai_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
